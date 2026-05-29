0cc2c16f53b95310b460980416b837c77f0ae5fb
MathisBesson <mathisbesson22@gmail.com>
2026-05-29 13:54:27 +0200

    Main menu Fonction

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // Index de ta scène de jeu
+        // Ou par nom : SceneManager.LoadScene("NomDeTaScene");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+}
